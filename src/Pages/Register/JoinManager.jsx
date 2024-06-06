6845f32b723884814216c35ea1620e5521b080db
Mohib <mohibullahmohim2020@gmail.com>
2024-06-06 20:32:53 +0600

    join as manager ui design okay

--- src/Pages/Register/JoinManager.jsx
+++ src/Pages/Register/JoinManager.jsx
@@ -6,103 +6,65 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import GoogleLogin from "../../Component/SocialLogin/GoogleLogin/GoogleLogin";
 import FacebookLogin from "../../Component/SocialLogin/FacebookLogin/FacebookLogin";
-import JoinManagerImg from "../../assets/join-manager-register.jpg"
 
 const JoinManager = () => {
-    const { createUser, updateProfileUser } = useAuth();
-    const navigate = useNavigate();
-    const [showPass, setShowPass] = useState(true);
-    const {
-      register,
-      formState: { errors },
-      handleSubmit,
-    } = useForm();
-    const photoURL =
-      "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
-  
-    const onSubmit = (data) => {
-      const name = data.name;
-      const email = data.email;
-      const password = data.password;
-  
-      // sign up
-      createUser(email, password)
-        .then((result) => {
-          console.log(result.user);
-  
-          updateProfileUser(name, photoURL).then(() => {
-            console.log("update profile");
-            const userInfo = {
-              name: name,
-              email: email,
-            };
-            console.log(userInfo, "user info");
-            navigate("/", { replace: true });
-          });
-        })
-        .catch((err) => {
-          console.error(err.message);
-          toast.error(err.message);
+  const { createUser, updateProfileUser } = useAuth();
+  const navigate = useNavigate();
+  const [showPass, setShowPass] = useState(true);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const photoURL =
+    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg";
+
+  const onSubmit = (data) => {
+    const name = data.name;
+    const email = data.email;
+    const password = data.password;
+
+    // sign up
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        updateProfileUser(name, photoURL).then(() => {
+          console.log("update profile");
+          const userInfo = {
+            name: name,
+            email: email,
+          };
+          console.log(userInfo, "user info");
+          navigate("/", { replace: true });
         });
-    };
-    return (
-        <div>
-        <HelmetTitle routeName={"Join Manager"}> </HelmetTitle>
-        <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-gray-200 py-10">
-          <div className="mt-3 w-full">
-            <p className="text-center font-bold text-3xl mb-10 ">
-              Please Register as a Manager{" "}
-            </p>
-          </div>
-          <div className="flex shadow-md">
+      })
+      .catch((err) => {
+        console.error(err.message);
+        toast.error(err.message);
+      });
+  };
+  return (
+    <div>
+      <HelmetTitle routeName={"Join Manager"}> </HelmetTitle>
+      <div className="flex flex-wrap min-h-screen w-full content-center justify-center pt-10 bg-[#f4f3f0]">
+        <div className="mt-3 w-full">
+          <p className="text-center font-bold text-3xl mb-10 ">Welcome back </p>
+        </div>
+        <div className="flex shadow-md pb-10 w-full">
+          <div className="flex flex-wrap content-center justify-center rounded-l-md w-full">
+            <div className="md:w-4/5">
+              <h1 className="text-xl font-semibold">
+                Please Register as a Manager
+              </h1>
+              <small className="text-gray-400">
+                Welcome back! Please enter your details
+              </small>
 
-            
-          <div
-              className="flex flex-wrap content-center justify-center rounded-r-md"
-              style={{ width: "24rem" }}
-            >
-              <img
-                className="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md"
-                src={JoinManagerImg}
-              />
-            </div>
-
-
-            <div
-              className="flex flex-wrap content-center justify-center rounded-l-md bg-white"
-              style={{ width: "24rem", }}
-            >
-              <div className="w-72">
-                <h1 className="text-xl font-semibold">Welcome back</h1>
-                <small className="text-gray-400">
-                  Welcome back! Please enter your details
-                </small>
-  
-                <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+              <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+                <div className="md:flex gap-4">
                   {/* Manager Name */}
-                  <div className="mb-3">
-                    <label className="mb-2 block text-xs font-semibold">
-                      Name
-                    </label>
-                    <input
-                      {...register("name", { required: true, maxLength: 20 })}
-                      type="text"
-                      name="name"
-                      placeholder="Enter your name"
-                      className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
-                    />
-                    {errors.name?.type === "required" && (
-                      <p className="text-red-600">Name is required</p>
-                    )}
-                    {errors.name?.type === "maxLength" && (
-                      <p className="text-red-600">
-                        Name should not be lenght 20 char
-                      </p>
-                    )}
-                  </div>
-                  
-                  {/* Company Name */}
-                  <div className="mb-3">
+                  <div className="mb-3 md:w-1/2">
                     <label className="mb-2 block text-xs font-semibold">
                       Name
                     </label>
@@ -123,22 +85,8 @@ const JoinManager = () => {
                     )}
                   </div>
 
-                {/* Company logo */}
-                <div className="mb-3">
-                <label className="mb-2 block text-xs font-semibold">
-                    Logo <span className="font-semibold text-sm text-gray-400" > (if you have) </span>
-                  </label>
-                  <input
-                  {...register('logo')}
-                  name="logo"
-                  type="file"
-                  accept="image/*"
-                  className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
-                  />
-                </div>
-
-                    {/* Email */}
-                  <div className="mb-3">
+                  {/* Email */}
+                  <div className="mb-3 md:w-1/2">
                     <label className="mb-2 block text-xs font-semibold">
                       Email
                     </label>
@@ -153,9 +101,11 @@ const JoinManager = () => {
                       <p className="text-red-600">Email is required</p>
                     )}
                   </div>
+                </div>
 
-                    {/* Password */}
-                  <div className="mb-3 relative ">
+                <div className="md:flex gap-4">
+                  {/* Password */}
+                  <div className="mb-3 relative md:w-1/2">
                     <label className="mb-2 block text-xs font-semibold">
                       Password
                     </label>
@@ -201,73 +151,125 @@ const JoinManager = () => {
                   </div>
 
                   {/* date of birth */}
-                <div className="mb-3">
-                  <label className="mb-2 block text-xs font-semibold">
-                    Date Of Birth
-                  </label>
-                  <input
-                    {...register("date", { required: true })}
-                    type="date"
-                    name="date"
-                    className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
-                  />
+                  <div className="mb-3 md:w-1/2">
+                    <label className="mb-2 block text-xs font-semibold">
+                      Date Of Birth
+                    </label>
+                    <input
+                      {...register("date", { required: true })}
+                      type="date"
+                      name="date"
+                      className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                    />
+                  </div>
+                </div>
+                <div className="md:flex gap-4">
+                  {/* Company Name */}
+                  <div className="mb-3 md:w-1/2">
+                    <label className="mb-2 block text-xs font-semibold">
+                      Company Name
+                    </label>
+                    <input
+                      {...register("name", { required: true, maxLength: 20 })}
+                      type="text"
+                      name="name"
+                      placeholder="Enter your name"
+                      className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                    />
+                    {errors.name?.type === "required" && (
+                      <p className="text-red-600">Name is required</p>
+                    )}
+                    {errors.name?.type === "maxLength" && (
+                      <p className="text-red-600">
+                        Name should not be lenght 20 char
+                      </p>
+                    )}
+                  </div>
+
+                  {/* Company logo */}
+                  <div className="mb-3 md:w-1/2">
+                    <label className="mb-2 block text-xs font-semibold">
+                      Logo{" "}
+                      <span className="font-semibold text-sm text-gray-400">
+                        {" "}
+                        (if you have){" "}
+                      </span>
+                    </label>
+                    <input
+                      {...register("logo")}
+                      name="logo"
+                      type="file"
+                      accept="image/*"
+                      className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                    />
+                  </div>
                 </div>
 
-                  {/* Packages Selection */}
+                {/* Packages Selection */}
                 <div className="mb-3">
                   <label className="mb-2 block text-xs font-semibold">
                     Select Member
                   </label>
-                  <select className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" {...register('member')} name="" id="">
+                  <select
+                    className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                    {...register("member")}
+                    name=""
+                    id=""
+                  >
                     <option value="member3"> 5 Members for $5 </option>
                     <option value="member10">10 Members for $8</option>
                     <option value="member20">20 Members for $15</option>
                   </select>
                 </div>
 
-                  {/* Submit */}
-                  <div className="mb-3">
-                    <input
-                      className={`mb-1.5 block w-full text-center text-white bg-blue-600 hover:bg-green-500 px-2 py-1.5 rounded-md cursor-pointer`}
-                      type="submit"
-                      value="Sign up"
-                    />
+                {/* Submit */}
+                <div className="mb-3">
+                  <input
+                    className={`mb-1.5 block w-full text-center text-white bg-blue-600 hover:bg-green-500 px-2 py-1.5 rounded-md cursor-pointer`}
+                    type="submit"
+                    value="Sign up"
+                  />
+                  <div className="md:flex gap-4 items-center">
+                    <div className="md:w-1/2">
                     <GoogleLogin></GoogleLogin>
+                    </div>
+                    <div className="md:w-1/2">
                     <FacebookLogin> </FacebookLogin>
+                    </div>
                   </div>
-                </form>
-  
-                <div className="text-center">
-                  <span className="text-xs text-gray-600 font-semibold">
-                    Already have an account?
-                  </span>
-                  <Link
-                    className="text-xs font-semibold text-purple-700"
-                    to="/login"
-                  >
-                    {" "}
-                    Sign in{" "}
-                  </Link>
                 </div>
-                <div className="text-center">
-                  <span className="text-xs text-gray-600 font-semibold">
-                    Don't want to join as a Manager?
-                  </span>
-                  <Link
-                    className="text-xs font-semibold text-purple-700"
-                    to="/join-employee"
-                  >
-                    {" "}
-                    Sign up{" "}
-                  </Link>
-                </div>
+              </form>
+
+              <div className="text-center">
+                <span className="text-xs text-gray-600 font-semibold">
+                  Already have an account?
+                </span>
+                <Link
+                  className="text-xs font-semibold text-purple-700"
+                  to="/login"
+                >
+                  {" "}
+                  Sign in{" "}
+                </Link>
+              </div>
+              <div className="text-center">
+                <span className="text-xs text-gray-600 font-semibold">
+                  Don't want to join as a Manager?
+                </span>
+                <Link
+                  className="text-xs font-semibold text-purple-700"
+                  to="/join-employee"
+                >
+                  {" "}
+                  Sign up{" "}
+                </Link>
               </div>
             </div>
-  
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default JoinManager;
