dd1388ee0173ec40edd55d43ff49c9898f5d8f9b
Mohib <mohibullahmohim2020@gmail.com>
2024-06-26 11:39:05 +0600

    Admin page customize with shadcn

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,11 +1,32 @@
 import useAllUser from "../../Hooks/useAllUser";
-
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "@/components/ui/resizable"
+   
 const Admin = () => {
     const [allUser] = useAllUser();
+    
     console.log('all user', allUser)
     return (
         <div>
-            <h1> hi i m admin </h1>
+            <<ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-[200px] max-w-md rounded-lg border"
+    >
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Sidebar</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={75}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Content</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
         </div>
     );
 };
